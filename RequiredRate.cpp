71163211a6984fdfb26db0dd810ebf53423f9194
Alim Uddin <mohammedalimuddin2004@gmail.com>
2023-10-09 11:24:37 +0600

    added a newline to wish best of luck

--- RequiredRate.cpp
+++ RequiredRate.cpp
@@ -1,9 +1,9 @@
-//After first portion of the lectures i am now here.. going to commit 'em for commit no.2
+// After first portion of the lectures i am now here.. going to commit 'em for commit no.2
 
 #include <bits/stdc++.h>
 using namespace std;
 int main()
-{ 
+{
     int target, overrem;
     cout << "Target: ";
 
@@ -12,6 +12,7 @@ int main()
     cout << "How many over is remaining?: ";
     cin >> overrem;
     cout << "Required Run Rate: " << (double)target / overrem << endl;
+    cout << "As i am going for third commit, i wish all the best to both of the teams" << endl;
 
     return 0;
 }
