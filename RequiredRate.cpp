ff7e868ab3fe605f0c236ff6b7d7660b85c78493
Alim Uddin <mohammedalimuddin2004@gmail.com>
2023-10-09 11:21:18 +0600

    added some features

--- RequiredRate.cpp
+++ RequiredRate.cpp
@@ -1,9 +1,9 @@
-//After first portion of the lectures i am now here.. going to commit 'em for commit no.2
+// It is now in a different branch where i will add some features
 
 #include <bits/stdc++.h>
 using namespace std;
 int main()
-{ 
+{
     int target, overrem;
     cout << "Target: ";
 
@@ -12,6 +12,7 @@ int main()
     cout << "How many over is remaining?: ";
     cin >> overrem;
     cout << "Required Run Rate: " << (double)target / overrem << endl;
-
+    // Added win percentage featuresdouble)target / overrem
+    cout << "Win percentage for batting team: " << (10 - round((double)target / overrem)) * 10 << "%" << endl;
     return 0;
 }
